9fe30b8ab4c6b2dd1156b5421f9fc8b0bacf9a29
Benjamin Wolski <ben.a.wolski@gmail.com>
2012-03-27 16:30:35 -0500

    Initial Commit

---
+++ gitHubExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitHubExample
+//
+//  Created by Ben Wolski on 3/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitHubExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gitHubExample
+//
+//  Created by Ben Wolski on 3/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
